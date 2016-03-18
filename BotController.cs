6f7389a94ecfd735580d8383eeb0f6426b68057b
Rogerson Nazário <rrnazario@gmail.com>
2016-03-18 17:10:06 -0300

    Add files
    Added a new feature: "Notify only once the same bug"

---
+++ BotController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BugRobot.Lib;
+
+namespace BugRobot.Web.Controllers
+{
+    public class BotController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetBugsFromTFS(string queryUrl, string userName, bool autoAssign)
+        {
+            var bugRobot = new BugRobot.Lib.BugRobot(queryUrl, userName, autoAssign);
+
+            var result = bugRobot.Run();
+
+            return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
