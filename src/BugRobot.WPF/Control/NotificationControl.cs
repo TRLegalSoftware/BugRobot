cf9e8d6fcb3adee306f23955b358d7bd23f93d11
Matheus Assis <adowtatep@hotmail.com.br>
2016-03-18 23:50:23 -0300

    Created a notification system

---
+++ src/BugRobot.WPF/Control/NotificationControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugRobot.WPF.Control
+{
+    public class NotificationControl
+    {
+        private NotifyIcon notificationSystem;
+        public NotificationControl()
+        {
+            this.notificationSystem.Icon = System.Drawing.SystemIcons.Warning;
+            this.notificationSystem.Visible = true;
+        }
+
+        private void setupNotification(string title, string text)
+        {
+            this.notificationSystem.BalloonTipTitle = title;
+            this.notificationSystem.BalloonTipText = text;
+        }
+
+        public void callNotification(string title, string text) {
+            this.setupNotification(title, text);
+            this.notificationSystem.ShowBalloonTip(0);
+        }
+
+        public void callNotification(string title, string text, int timeout) {
+            this.setupNotification(title, text);
+            this.notificationSystem.ShowBalloonTip(timeout);
+        }
+    }
+}
